ac3b0672d0887dd3a0af390bcbea54574081fe8c
Marcia <marcia.garciam@hotmail.com>
2022-05-19 16:32:53 -0500

    clearing form data if inputs are correct

--- JavaScript-Lab Resources/Form Validation/scripts/app.js
+++ JavaScript-Lab Resources/Form Validation/scripts/app.js
@@ -14,6 +14,13 @@ clearErrors = () => {
     fieldInputEmail.nextElementSibling.textContent = 'Email cannot be empty';
 }
 
+clearFormData = () => {
+    fieldInputUserName.value = '';
+    fieldInputLastName.value = '';
+    fieldInputEmail.value = '';
+    fieldInputPwd.value = '';
+}
+
 //Validate email
 isEmailValid = (email) => {
     const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -24,6 +31,7 @@ validateFormData = (e) => {
     // I am not really submitting data to a backend
     e.preventDefault();
     clearErrors();
+    let errorsFound = false;
     // grab the value of each input
     const firstName = fieldInputUserName.value;
     const lastName = fieldInputLastName.value;
@@ -33,26 +41,35 @@ validateFormData = (e) => {
     if (firstName === '') {
         // if first name empty then unset the css attribute visibility
         fieldInputUserName.nextElementSibling.setAttribute('style', 'visibility:unset;');
+        errorsFound = true;
     }
     // check last name
     if (lastName === '') {
         // if last name empty then unset the css attribute visibility
         fieldInputLastName.nextElementSibling.setAttribute('style', 'visibility:unset;');
+        errorsFound = true;
     }
     // check email
     if (email === '') {
         // if email empty then unset the css attribute visibility
         fieldInputEmail.nextElementSibling.setAttribute('style', 'visibility:unset');
+        errorsFound = true;
     } else if (!isEmailValid(email)) {
         // if not valid email format then unset the css attribute visibility
         // and change the error message text
         fieldInputEmail.nextElementSibling.textContent = 'Looks like this is not a valid email';
         fieldInputEmail.nextElementSibling.setAttribute('style', 'visibility:unset');
+        errorsFound = true;
     }
     // check password
     if (password === '') {
         // if password empty then unset the css attribute visibility
         fieldInputPwd.nextElementSibling.setAttribute('style', 'visibility:unset;');
+        errorsFound = true;
+    }
+    // clear form data if no erros found
+    if (!errorsFound) {
+        clearFormData();
     }
 };
 
