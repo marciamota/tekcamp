c6d53f7093b6beed7601c646146733f1b3e0b189
Marcia <marcia.garciam@hotmail.com>
2022-06-01 20:47:19 -0500

    login page html added

--- e-commerce-react/src/pages/LoginPage.js
+++ e-commerce-react/src/pages/LoginPage.js
@@ -6,9 +6,20 @@ import classes from './LoginPage.module.css';
 
 const LoginPage = (props) => {
     return (
-        <div className={'ui cards ' + classes.productrow}>
-            {/* {productCards} */}
-            test
+        <div className={'ui container'}>
+            <div className={classes.containerWidth}>
+                <form className="ui form">
+                    <div className="field">
+                        <label>User Name</label>
+                        <input type="text" placeholder="User Name" />
+                    </div>
+                    <div class="field">
+                        <label>Password</label>
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <button class="ui button" type="submit">Login</button>
+                </form>
+            </div>
         </div>
     );
 };
